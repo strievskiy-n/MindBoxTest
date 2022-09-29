346b1f6a083909587f2ac982537b40736b98bb43
strievskiy-n <38487752+strievskiy-n@users.noreply.github.com>
2022-09-29 15:57:28 +0300

    Update

--- SQL_QUERY.sql
+++ SQL_QUERY.sql
@@ -1,1 +1,1 @@
-SELECT p.productName, c.categoryName FROM ProductTale p LEFT JOIN CategoryTale c ON p.productName = c.productName
+SELECT p.productName, c.categoryName FROM ProductTale p LEFT JOIN CategoryTale c ON p.productName = c.productName ORDER BY p.productName
